00067490eec33ea8524ac85523ad2c5c1489cf30
Pedro Santos <2140269@my.ipleiria.pt>
2018-12-11 11:47:09 +0000

    Atualização vista

--- resources/views/vue/index.blade.php
+++ resources/views/vue/index.blade.php
@@ -6,8 +6,10 @@
 
           <router-link to="/items">Menu</router-link> -           
           <router-link to="/users">Users</router-link> -
-          <router-link to="/orders">Orders</router-link>
-
+          <router-link to="/orders">Orders</router-link> -
+          <router-link to="/profile">Profile</router-link> -
+          <router-link to="/login">Login</router-link> -
+          <router-link to="/logout">Logout</router-link>
           <router-view> </router-view>
 
 @endsection
