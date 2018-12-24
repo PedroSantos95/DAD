498521b23c4619ff0a20fb6441377dee38e69e97
Joao <2140259@my.ipleiria.pt>
2018-12-24 02:38:32 +0000

    commit

--- app/Http/Controllers/ItemControllerAPI.php
+++ app/Http/Controllers/ItemControllerAPI.php
@@ -1,26 +1,37 @@
 <?php
 namespace App\Http\Controllers;
-use App\Invoice;
-use App\Meals;
+use App\Item;
 use Illuminate\Http\Request;
-use App\Http\Resources\Invoice as InvoiceResource;
-use DB;
-class InvoiceControllerAPI extends Controller
+use App\Http\Resources\Item as ItemResource;
+use Illuminate\Support\Facades\DB;
+class ItemControllerAPI extends Controller
 {
- 
-    public function getInvoices(Request $request)
+    public function getItems(Request $request)
     {
-        return InvoiceResource::collection(Invoice::all());
+        return ItemResource::collection(Item::all());
+        
     }
- 
-    public function showInvoice(Request $request, $id){
-        $items = Invoice::findOrFail($id);
- 
-        $items = DB::table('invoice_items')
-            ->join('items', 'invoice_items.item_id', '=', 'items.id')
-            ->select('invoice_items.quantity', 'invoice_items.unit_price', 'invoice_items.sub_total_price', 'items.name')
-            ->where('invoice_items.invoice_id', $id)->get();
-         return $items;
+    public function add(Request $request)
+    {
+        $item = new Item();
+        $item->fill($request->all()); 
+        $item->photo_url= "dsfds"; 
+        $item->save();
+        
+       return response()->json($item,200);
+    } 
+    public function edit(Request $request)
+    {
+        $id = $request->query('id');
+        $item = Item::findOrFail($id);
+        $item->update($request->all());
+        return response()->json($item,200);
     }
- 
+    public function destroy(Request $request)
+    {
+        $item = Item::findOrFail($id);
+        $item->delete();
+        return response()->json(null, 204);
+    }
+   
 }
